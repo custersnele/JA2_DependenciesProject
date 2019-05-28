e01c8e711e2c997ebcbf4c7e2e746d99a64f245c
PXLPERS\20003575 <custersnele@gmail.com>
2019-05-28 19:05:32 +0200

    inital version

---
+++ src/test/java/be/pxl/DummyTest.java
@@ -0,0 +1,14 @@
+package be.pxl;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class DummyTest {
+
+    @Test
+    public void alwaysTrue() {
+        assertTrue(true);
+    }
+}
